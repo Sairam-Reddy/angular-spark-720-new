797eb198f737782504365cde4b193ee2e8569f52
Sairam-Reddy <sairamreddyg33@gmail.com>
2020-09-14 17:34:50 +0530

    5 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import "@progress/kendo-ui";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { DraftWorkflowModule } from "../common/workflow/draft/draft-worklfow.module";
+import { ViewWorkflowModule } from "../common/workflow/create/view-workflow.module";
+import { AngularFireModule } from 'angularfire2'; 
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { environment } from './environment';
+import { WorkflowService } from "../common/workflow/services/workflow.service";
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatToolbarModule,
+    ViewWorkflowModule,
+    DraftWorkflowModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [AngularFirestore, WorkflowService]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDFFGeVCnuC2xd3bZbluUEY9VzKH0ta3kI",
+    authDomain: "spark-720.firebaseapp.com",
+    databaseURL: "https://spark-720.firebaseio.com",
+    projectId: "spark-720",
+    storageBucket: "spark-720.appspot.com",
+    messagingSenderId: "42329418698",
+    appId: "1:42329418698:web:6c9666b4751dc49d1b3d33",
+    measurementId: "G-Z3DQVR4V51"
+  }
+};
