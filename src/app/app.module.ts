4d61f53cefc8df41774ca8a094b9328ef92bc0b9
Sairam-Reddy <sairamreddyg33@gmail.com>
2020-09-14 17:51:29 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,6 @@ import { AppComponent } from "./app.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { DraftWorkflowModule } from "../common/workflow/draft/draft-worklfow.module";
 import { ViewWorkflowModule } from "../common/workflow/create/view-workflow.module";
-import { AngularFireModule } from 'angularfire2'; 
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFirestore } from 'angularfire2/firestore';
 import { environment } from './environment';
 import { WorkflowService } from "../common/workflow/services/workflow.service";
 
@@ -23,7 +20,6 @@ import { WorkflowService } from "../common/workflow/services/workflow.service";
     DraftWorkflowModule
   ],
   declarations: [AppComponent],
-  bootstrap: [AppComponent],
-  providers: [AngularFirestore, WorkflowService]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
